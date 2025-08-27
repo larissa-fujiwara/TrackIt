17a9eeb79e0f93ff0edddb1ea00a36bbb19fd5a8
fuji <lrs.fujiwara@gmail.com>
2025-08-27 20:37:32 -0300

    Adiciona estilo para link e button

--- src/sharedStyles/stylesSetUp.js
+++ src/sharedStyles/stylesSetUp.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import styled from "styled-components";
 
 export const Content = styled.main`
@@ -6,14 +7,42 @@ export const Content = styled.main`
     flex-direction: column;
     align-items: center;
     padding: 60px 20px;
-    background-color: red;
+    background-color: #FFFF;
 `
+
+export const Form = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    row-gap: 8px;
+    @media (min-width: 450px) {
+        max-width:450px;
+    }
+`
+
 export const Input = styled.input`
     width: 100%;
-    height: 45px;
-    border: 1px solid #D4D4D4;
+    height: 2.75rem;
+    font-size: 1.25rem;
+    padding: 8px;
+    border: 2px solid #D4D4D4;
     border-radius: 4px;
-    &::placeholder{
-        padding: 0 8px;
+    transition: 0.4s;
+    outline: none;
+    &:focus{
+        border: 2px solid #666666;
     }
+`
+
+export const Button = styled.button`
+    height: 2.75rem;
+    font-size: 1.25rem;
+    border-radius: 4px;
+    border: none;
+    background-color: #52B6FF;
+    color: #FFFFFF;
+`
+export const NavLink = styled(Link)`
+    color:#52B6FF;
 `
