35ebe7b65e644aaceb91e9e8a194032547802685
fuji <lrs.fujiwara@gmail.com>
2025-09-01 15:54:40 -0300

    Aplica Layout Routes para o componente HomePage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import HabitsPage from "./pages/HabitsPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import Today from "./components/Today";
+import Habits from "./components/Habits";
+import { useNavigate } from "react-router";
 
 export default function App() {
+
+  /* const navigate = useNavigate(); */
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/sign-up' element={<SignUpPage />} />
-        <Route path='/habits' element={<HabitsPage />} />
+        <Route element={<HomePage />}>
+          <Route path="today" element={<Today />} />
+          <Route path="habits" element={<Habits />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
