cea8c5acb4d7ac4732b3f59b7069b90f53850c2f
fuji <lrs.fujiwara@gmail.com>
2025-09-06 21:32:40 -0300

    Instala axios, Adiciona requisição POST para cadastro de novo usuário e login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,27 +1,48 @@
-import { Content, Form, Input, Button, ActionLink} from "../sharedStyles/stylesSetUp.js";
+import { Content, Form, Input, Button, ActionLink } from "../sharedStyles/stylesSetUp.js";
 import { Emblem } from "../sharedStyles/imagesSetUp.js";
 import emblem from "../assets/emblem.svg";
+import { useState } from "react";
+import axios from "axios";
 
 export default function LoginPage() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function logOn(e) {
+        e.preventDefault();
+
+        const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login';
+        const body = {email, password};
+
+        axios.post(URL, body)
+            .then(res => console.log(res.data))
+            .catch(err => alert(err.response.data.message))
+    }
+
     return (
         <Content>
             <Emblem src={emblem} alt="TrackIt Emblem" />
-            <Form>
+            <Form onSubmit={logOn}>
                 <Input
-                    required 
+                    required
                     type="email"
-                    placeholder="seuemail@email.com"     
+                    placeholder="seuemail@email.com"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
-                    required 
+                    required
                     type="password"
-                    placeholder="senha"  
+                    placeholder="senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                 />
-                
+
                 <Button type="submit">Entrar</Button>
             </Form>
-            <ActionLink>Não tem conta? Cadastre-se!</ActionLink>
+            <ActionLink to='/sign-up'>Não tem conta? Cadastre-se!</ActionLink>
         </Content>
     )
 }
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,39 +1,70 @@
 import { Content, Form, Input, Button, ActionLink} from "../sharedStyles/stylesSetUp.js";
 import { Emblem } from "../sharedStyles/imagesSetUp.js";
 import emblem from "../assets/emblem.svg";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router";
+
 
 export default function SignUpPage(){
+    
+    const [email, setEmail] = useState('');
+    const [image, setImage] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    function createAccount(e){
+        e.preventDefault();
+        
+        const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up';
+        const body = {email, name, image, password};
+        
+        axios.post(URL, body)
+            .then(() => navigate('/'))
+            .catch(err => alert(err.response.data.message))
+        
+    }
+    
     return (
         <Content>
             <Emblem src={emblem} alt="TrackIt Emblem" />
-            <Form>
+            <Form onSubmit={createAccount}>
                 <Input
                     required 
                     type="email"
-                    placeholder="seuemail@email.com"     
+                    placeholder="seuemail@email.com"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}     
                 />
 
                 <Input
                     required 
                     type="password"
-                    placeholder="senha"  
+                    placeholder="senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}  
                 />
 
                 <Input
                     required 
                     type="text"
-                    placeholder="nome"  
+                    placeholder="nome" 
+                    value={name}
+                    onChange={e => setName(e.target.value)} 
                 />
 
                 <Input
                     required 
                     type="url"
-                    placeholder="foto"  
+                    placeholder="foto"
+                    value={image}
+                    onChange={e => setImage(e.target.value)}  
                 />
                 
                 <Button type="submit">Cadastrar</Button>
             </Form>
-            <ActionLink>Já tem uma conta? Faça login!</ActionLink>
+            <ActionLink to='/'>Já tem uma conta? Faça login!</ActionLink>
         </Content>
     )
 }
