238c2b7f7b396417d56b6438477d3a8ffe1bccc4
fuji <lrs.fujiwara@gmail.com>
2025-09-12 15:21:23 -0300

    Adiciona componente e interatividade de seleção de dias da semana

---
+++ src/components/Weekday.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styled from "styled-components"
+
+export default function Weekday ({day, dayNum, days, setDays}) {
+
+    const [isSelected, setIsSelected] = useState(false);
+
+    function updateDays(){
+
+        if(days.includes(dayNum)){
+            setDays(days.filter(day => day !== dayNum));
+            setIsSelected(false);
+
+        }else{
+            setDays([...days, dayNum]);
+            setIsSelected(true);
+        }
+
+    }
+
+
+    return (
+        <DayOption $isSelected={isSelected} type="button" onClick={updateDays}>{day}</DayOption>
+    )
+}
+
+const DayOption = styled.button`
+    width: 100%;
+    border-radius: 4px;
+    border: 2px solid #D4D4D4;
+    color:${({$isSelected}) => $isSelected ? '#FFFFFF' : '#D4D4D4' } ;
+    background-color: ${({$isSelected}) => $isSelected ? '#CFCFCF' : 'transparent' };
+    
+`
